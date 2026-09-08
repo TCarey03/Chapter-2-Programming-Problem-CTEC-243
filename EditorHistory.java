00267a48a9ca5c8c7c327b0c3e9f46f81908228d
TCarey03 <tylercarey03@gmail.com>
2026-09-07 21:35:09 -0500

    Add redo functionality to EditorHistory class
    Phase 3 completed.

--- EditorHistory.java
+++ EditorHistory.java
@@ -1,21 +1,34 @@
 public class EditorHistory {
 
     private Stack<String> undoStack;
+    private Stack<String> redoStack;
     private String currentState;
 
     public EditorHistory() {
         undoStack = new Stack<>();
+        redoStack = new Stack<>();
         currentState = "";
     }
 
     public void makeChange(String newState) {
         undoStack.push(currentState);
         currentState = newState;
+
+        // A new change makes the old redo history invalid
+        redoStack = new Stack<>();
     }
 
     public void undo() {
         if (!undoStack.isEmpty()) {
+            redoStack.push(currentState);
             currentState = undoStack.pop();
+        }
+    }
+
+    public void redo() {
+        if (!redoStack.isEmpty()) {
+            undoStack.push(currentState);
+            currentState = redoStack.pop();
         }
     }
 
